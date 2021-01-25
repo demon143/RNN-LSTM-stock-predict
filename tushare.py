342a94314c6a5ef747a83eedbc1514de1b5738ca
demon143 <785696300@qq.com>
2021-01-25 14:42:34 +0800

    update

---
+++ tushare.py
@@ -0,0 +1,8 @@
+import tushare as ts
+import matplotlib.pyplot as plt
+
+#提取数据的代码#
+df1 = ts.get_k_data('600519', ktype='D', start='2010-04-26', end='2020-04-26')
+
+datapath1 = "./SH600519.csv"
+df1.to_csv(datapath1)
